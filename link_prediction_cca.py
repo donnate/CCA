ad9af4f6d034bd603bea706a291fc618c5a6cee1
donnate <cdonnat@uchicago.edu>
2022-05-10 17:17:41 -0500

    fix issue with the adjacency  matrix in cca, plus add the option to do negative sampling for the edges

--- link_prediction_cca.py
+++ link_prediction_cca.py
@@ -15,19 +15,35 @@ import torch_geometric.transforms as T
 from torch_geometric.transforms import NormalizeFeatures
 from torch_geometric.utils import to_dense_adj
 
+from torch_geometric.utils import (
+    add_self_loops,
+    negative_sampling,
+    remove_self_loops,
+)
+
 from model import *
 from aug import *
+
+
+def parse_boolean(value):
+    value = value.lower()
+    if value in ["true", "yes", "y", "1", "t"]:
+        return True
+    elif value in ["false", "no", "n", "0", "f"]:
+        return False
+    return False
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='VGNAE')
 parser.add_argument('--dataset', type=str, default='Cora')
 parser.add_argument('--epochs', type=int, default=300)
-parser.add_argument('--n_experiments', type=int, default=1)
+parser.add_argument('--n_experiments', type=int, default=50)
 parser.add_argument('--n_layers', type=int, default=2)
 parser.add_argument('--channels', type=int, default=128)
-parser.add_argument('--normalize', type=bool, default=True)
-parser.add_argument('--non_linear', type=bool, default=True)
+parser.add_argument('--normalize', type=parse_boolean, default=True)
+parser.add_argument('--non_linear', type=parse_boolean, default=True)
+parser.add_argument('--add_negative_samples', type=parse_boolean, default=False)
 parser.add_argument('--scaling_factor', type=float, default=1.8)
 parser.add_argument('--lr', type=float, default=0.0005)
 parser.add_argument('--drop_rate_edge', type=float, default=0.2)
@@ -58,6 +74,7 @@ dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 in_dim = data.num_features
 N = data.num_nodes
 
+print(args.add_negative_samples)
 
 from sklearn.metrics import roc_auc_score, average_precision_score
 def compute_loss_para(adj):
@@ -100,105 +117,131 @@ for training_rate in [0.1, 0.2, 0.4, 0.6, 0.8, 0.85]:
     val_ratio = (1.0 - training_rate) / 3
     test_ratio = (1.0 - training_rate) / 3 * 2
     for exp in range(args.n_experiments):
-        model = CCA_SSG(in_dim, hid_dim, out_dim, n_layers, use_mlp=False)
-        lr1 = args.lr
-        wd1 = 0
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr1, weight_decay=wd1)
-        for epoch in range(25):
-            model.train()
-            optimizer.zero_grad()
+        for lambd in np.logspace(-7, 2, num=10, endpoint=True, base=10.0, dtype=None, axis=0):
+            model = CCA_SSG(in_dim, hid_dim, out_dim, n_layers, use_mlp=False)
+            lr1 = args.lr
+            wd1 = 0
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr1, weight_decay=wd1)
+            for epoch in range(25):
+                model.train()
+                optimizer.zero_grad()
 
-            dfr = args.drop_rate_feat
-            der =args.drop_rate_edge
+                dfr = args.drop_rate_feat
+                der =args.drop_rate_edge
 
-            new_data1 = random_aug(data, dfr , der )
-            new_data2 = random_aug(data, dfr, der)
+                new_data1 = random_aug(data, dfr , der )
+                new_data2 = random_aug(data, dfr, der)
 
-            z1, z2 = model(new_data1, new_data2)
+                z1, z2 = model(new_data1, new_data2)
 
-            c = torch.mm(z1.T, z2)
-            c1 =torch.mm(z1.T, z1)
-            c2 = torch.mm(z2.T, z2)
+                c = torch.mm(z1.T, z2)
+                c1 =torch.mm(z1.T, z1)
+                c2 = torch.mm(z2.T, z2)
 
-            c = c / N
-            c1 = c1 / N
-            c2 = c2 / N
+                c = c / N
+                c1 = c1 / N
+                c2 = c2 / N
 
-            loss_inv = -torch.diagonal(c).sum()
-            iden = torch.tensor(np.eye(c.shape[0]))
-            loss_dec1 = (iden - c1).pow(2).sum()
-            loss_dec2 = (iden - c2).pow(2).sum()
+                loss_inv = -torch.diagonal(c).sum()
+                iden = torch.tensor(np.eye(c.shape[0]))
+                loss_dec1 = (iden - c1).pow(2).sum()
+                loss_dec2 = (iden - c2).pow(2).sum()
 
-            lambd = 1e-3
+                loss = loss_inv + lambd * (loss_dec1 + loss_dec2)
 
-            loss = loss_inv + lambd * (loss_dec1 + loss_dec2)
+                loss.backward()
+                optimizer.step()
 
-            loss.backward()
-            optimizer.step()
+                print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss.item()))
 
-            print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss.item()))
-
-        # In[ ]:
+            # In[ ]:
 
 
-        print("=== Evaluation ===")
-        data = dataset[0]
-        transform = RandomLinkSplit(num_val=val_ratio, num_test=test_ratio,
-                                    is_undirected=True, split_labels=True)
-        train_data, val_data, test_data = transform(data)
-        embeds = model.get_embedding(train_data)
-        adj_train = to_dense_adj(train_data.edge_index)
-        adj_train = adj_train[0]
-        print(embeds.shape, adj_train.shape)
-        weight_tensor, norm = compute_loss_para(adj_train)
-        logreg = LogReg(embeds.shape[1], adj_train.shape[1])
-        lr2 = 1e-3
-        wd2 = 1e-4
-        opt = torch.optim.Adam(logreg.parameters(), lr=lr2, weight_decay=wd2)
+            print("=== Evaluation ===")
+            data = dataset[0]
+            transform = RandomLinkSplit(num_val=val_ratio, num_test=test_ratio,
+                                        is_undirected=True, split_labels=True)
+            train_data, val_data, test_data = transform(dataset[0])
+            embeds = model.get_embedding(train_data)
+            adj_train = to_dense_adj(train_data.edge_index,  max_num_nodes=N)
+            adj_train = adj_train[0]
+            #print(embeds.shape, adj_train.shape)
+            weight_tensor, norm = compute_loss_para(adj_train)
+            logreg = LogReg(embeds.shape[1], adj_train.shape[1])
+            lr2 = 1e-3
+            wd2 = 1e-4
+            opt = torch.optim.Adam(logreg.parameters(), lr=lr2, weight_decay=wd2)
 
-        loss_fn = F.binary_cross_entropy
-        output_activation = nn.Sigmoid()
+            loss_fn = F.binary_cross_entropy
+            output_activation = nn.Sigmoid()
 
-        best_val_roc = 0
-        eval_roc = 0
-        best_val_ap = 0
-        eval_ap = 0
+            best_val_roc = 0
+            eval_roc = 0
+            best_val_ap = 0
+            eval_ap = 0
 
-        for epoch in range(args.epochs):
-            logreg.train()
-            opt.zero_grad()
-            logits_temp = logreg(embeds)
-            logits = output_activation(torch.mm(logits_temp, logits_temp.t()))
-            print(logits.view(-1).shape, adj_train.view(-1).shape, weight_tensor.shape)
+            for epoch in range(args.epochs):
+                logreg.train()
+                opt.zero_grad()
 
-            # pdb.set_trace()
-            loss = norm * loss_fn(logits.view(-1), adj_train.view(-1), weight = weight_tensor)
-            loss.backward()
-            opt.step()
+                #### 1st alternative:
+                logits_temp = logreg(embeds)
+                logits = output_activation(torch.mm(logits_temp, logits_temp.t()))
+                #print(logits.view(-1).shape, adj_train.view(-1).shape, weight_tensor.shape)
 
-            logreg.eval()
-            with torch.no_grad():
-                val_roc, val_ap = get_scores(val_data.pos_edge_label_index, val_data.neg_edge_label_index, logits)
-                test_roc, test_ap = get_scores(test_data.pos_edge_label_index, test_data.neg_edge_label_index, logits)
 
-                if val_roc >= best_val_roc:
-                    best_val_roc = val_roc
-                    if test_roc > eval_roc:
-                        eval_roc = test_roc
+                # pdb.set_trace()
+                #z = torch.tensor([logits[e[0], e[1]] for e in pos_edge_index.T]+ [logits[e[0], e[1]] for e in neg_edge_index.T])
+                #true_z = torch.from_numpy(np.hstack([np.ones(pos_edge_index.shape[1]), np.zeros(neg_edge_index.shape[1])])).float()
+                #loss = norm * loss_fn(z, true_z)
+                if args.add_negative_samples == False:
+                    loss = norm*loss_fn(logits.view(-1), adj_train.view(-1), weight = weight_tensor)
+                else:
+                    it = 0
+                    pos_edge_index, _ = remove_self_loops(train_data.pos_edge_label_index)
+                    pos_edge_index, _ = add_self_loops(pos_edge_index)
+                    neg_edge_index = negative_sampling(pos_edge_index, N)
+                    for e in pos_edge_index:
+                        if it == 0:
+                            loss = -norm * torch.log(logits[e[0], e[1]])
+                        else:
+                            loss += -norm * torch.log(logits[e[0], e[1]])
+                        it += 1
+                    for e in neg_edge_index:
+                            loss += -norm * torch.log(1-logits[e[0], e[1]])
 
-                if val_ap >= best_val_ap:
-                    best_val_ap = val_ap
-                    if test_ap > eval_ap:
-                        eval_ap = test_ap
+                # neg_loss = -torch.log(1 - torch.tensor([logits[e[0], e[1]] for e in pos_edge_index.T])).mean()
+                #
 
-            print('Epoch:{}, val_ap:{:.4f}, val_roc:{:4f}, test_ap:{:4f}, test_roc:{:4f}'.format(epoch, val_ap, val_roc, test_ap, test_roc))
-            print('Linear evaluation AP:{:.4f}'.format(eval_ap))
-            print('Linear evaluation ROC:{:.4f}'.format(eval_roc))
-            results += [[exp, 'CCA', args.dataset, True, True, args.lr, args.channels,
-                                  training_rate, val_ratio, test_ratio, 0, test_roc, test_ap, epoch, args.drop_rate_edge, args.drop_rate_feat]]
-            res1 = pd.DataFrame(results, columns=['exp', 'model', 'dataset', 'non-linearity', 'normalize',  'lr', 'channels',
-                                                  'train_rate', 'val_ratio',
-                                                  'test_ratio', 'alpha', 'auc', 'ap', 'epoch', 'drop_edge_rate', 'drop_feat_rate'])
-            res1.to_csv(file_path  +  args.model + str(args.non_linear) + "_norm" +  str(args.normalize) +  "_lr"+ str(args.lr) +
-                        '_channels' + str(args.channels) + "_dropedgerate" + str( args.drop_rate_edge) + "_dropfeatrate" + str( args.drop_rate_feat)+
-                        ".csv", index=False)
+
+                loss.backward()
+                opt.step()
+
+
+                logreg.eval()
+                with torch.no_grad():
+                    val_roc, val_ap = get_scores(val_data.pos_edge_label_index, val_data.neg_edge_label_index, logits)
+                    test_roc, test_ap = get_scores(test_data.pos_edge_label_index, test_data.neg_edge_label_index, logits)
+
+                    if val_roc >= best_val_roc:
+                        best_val_roc = val_roc
+                        if test_roc > eval_roc:
+                            eval_roc = test_roc
+
+                    if val_ap >= best_val_ap:
+                        best_val_ap = val_ap
+                        if test_ap > eval_ap:
+                            eval_ap = test_ap
+
+                print('Epoch:{}, val_ap:{:.4f}, val_roc:{:4f}, test_ap:{:4f}, test_roc:{:4f}'.format(epoch, val_ap, val_roc, test_ap, test_roc))
+                print('Linear evaluation AP:{:.4f}'.format(eval_ap))
+                print('Linear evaluation ROC:{:.4f}'.format(eval_roc))
+                results += [[exp, 'CCA', args.dataset, True, True, args.lr, args.channels,
+                                      training_rate, val_ratio, test_ratio, lambd, test_roc, test_ap, epoch, args.drop_rate_edge, args.drop_rate_feat]]
+                res1 = pd.DataFrame(results, columns=['exp', 'model', 'dataset', 'non-linearity', 'normalize',  'lr', 'channels',
+                                                      'train_rate', 'val_ratio',
+                                                      'test_ratio', 'alpha', 'auc', 'ap', 'epoch', 'drop_edge_rate', 'drop_feat_rate'])
+                res1.to_csv(file_path  +  args.model + str(args.non_linear) + "_norm" +  str(args.normalize) +  "_lr"+ str(args.lr) +
+                            '_channels' + str(args.channels) + "_dropedgerate" + str( args.drop_rate_edge) + "_dropfeatrate" +
+                            str( args.drop_rate_feat)+ 'with_neg_samples' + str(args.add_negative_samples) +
+                            ".csv", index=False)
--- link_prediction_experiment.py
+++ link_prediction_experiment.py
@@ -14,14 +14,24 @@ import torch_geometric.transforms as T
 from VGNAE import *
 from GeneralizedVGAE import *
 
+def parse_boolean(value):
+    value = value.lower()
+    if value in ["true", "yes", "y", "1", "t"]:
+        return True
+    elif value in ["false", "no", "n", "0", "f"]:
+        return False
+    return False
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='VGNAE')
 parser.add_argument('--dataset', type=str, default='Cora')
 parser.add_argument('--epochs', type=int, default=300)
 parser.add_argument('--n_experiments', type=int, default=1)
 parser.add_argument('--channels', type=int, default=128)
-parser.add_argument('--normalize', type=bool, default=True)
-parser.add_argument('--non_linear', type=bool, default=True)
+parser.add_argument('--patience', type=int, default=10)
+parser.add_argument('--normalize', type=parse_boolean, default=True)
+parser.add_argument('--non_linear', type=parse_boolean, default=True)
 parser.add_argument('--scaling_factor', type=float, default=1.8)
 parser.add_argument('--lr', type=float, default=0.0005)
 parser.add_argument('--result_file', type=str, default="/results/results_link_prediction_")
@@ -58,11 +68,17 @@ def train(model, optimizer, train_data, model_type):
     optimizer.step()
     return loss
 
-def test(model, optimizer, train_data, test_data):
+def test(model, train_data, test_data):
     model.eval()
     with torch.no_grad():
         z = model.encode(train_data.x, train_data.pos_edge_label_index)
     return model.test(z, test_data.pos_edge_label_index, test_data.neg_edge_label_index)
+
+def validation(model, train_data, val_data):
+    model.eval()
+    with torch.no_grad():
+        z = model.encode(train_data.x, train_data.pos_edge_label_index)
+    return model.test(z, val_data.pos_edge_label_index, val_data.neg_edge_label_index)
 
 results =[]
 for training_rate in [0.1, 0.2, 0.4, 0.6, 0.8, 0.85]:
@@ -94,13 +110,33 @@ for training_rate in [0.1, 0.2, 0.4, 0.6, 0.8, 0.85]:
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
             model = model.to(device)
             optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+            last_loss = 1e10
+            patience = args.patience
+            triggertimes = 0
+
             for epoch in range(1, args.epochs):
                 loss = train(model, optimizer, train_data, args.model)
                 loss = float(loss)
                 with torch.no_grad():
                     test_pos, test_neg = train_data.pos_edge_label_index, train_data.neg_edge_label_index
-                    auc, ap = test(model, optimizer, train_data, test_data)
+                    auc, ap = test(model, train_data, test_data)
                     print('Epoch: {:03d}, LOSS: {:.4f}, AUC: {:.4f}, AP: {:.4f}'.format(epoch, loss, auc, ap))
+
+
+                #### Add early stopping to prevent overfitting
+                out = validation(model, train_data, val_data)
+                current_loss = out[1]
+                if current_loss >= last_loss:
+                    trigger_times += 1
+                    #print('Trigger Times:', trigger_times)
+                    if trigger_times >= patience:
+                        #print('Early stopping!\nStart to test process.')
+                        break
+                else:
+                    #print('trigger times: 0')
+                    trigger_times = 0
+                last_loss = current_loss
             results += [[exp, args.model, args.dataset, args.non_linear, args.normalize, args.lr, args.channels,
                                   training_rate, val_ratio, test_ratio, alpha, auc, ap, epoch]]
             res1 = pd.DataFrame(results, columns=['exp', 'model', 'dataset', 'non-linearity', 'normalize',  'lr', 'channels',
